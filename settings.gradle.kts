d3a0302b9fb1d57839839f74570e7fbbe71899f9
Neo <c.min.tseng@gmail.com>
2024-04-20 02:50:52 +0800

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,48 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven(url = "https://plugins.gradle.org/m2/")
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven{
+            url "https://raw.githubusercontent.com/cybernhl/maven-repository/master/"
+        }
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven{
+            url "https://raw.githubusercontent.com/cybernhl/maven-repository/master/"
+        }
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+val fullVersion = System.getProperty("java.version", "8.0.0")
+val versionComponents = fullVersion
+    .split(".")
+    .take(2)
+    .filter { it.isNotBlank() }
+    .map { Integer.parseInt(it) }
+
+val currentJdk = if (versionComponents[0] == 1) versionComponents[1] else versionComponents[0]
+
+rootProject.name = "frpc_mobile"
